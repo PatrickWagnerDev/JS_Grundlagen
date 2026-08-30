1f6028f5a6dab83331ba9973cc6b84a3b3c24ccc
Patrick Wagner <patrickwagner2607@gmail.com>
2026-08-30 11:17:22 +0200

    add: define the Exercises

---
+++ practice.js
@@ -0,0 +1,26 @@
+// 01 Aufgabe
+// Erstelle eine Funktion namens "fullName", die zwei Strings als Parameter übernimmt,
+// sie zusammenfügt zu einem einzigen String und diesen zurückgibt (returned).
+// Beispiel: console.log(fullName('Max','Mustermann')); // Ausgabe: Max Mustermann
+
+
+
+// 02 Aufgabe
+// Erstelle eine Funktion namens "capitalizeLetters", die einen String als Parameter übernimmt,
+// jeden Buchstaben darin groß schreibt und diesen dann wieder zurückgibt.
+// Beispiel: console.log(capitalizeLetters('bAnanE')); // Ausgabe: BANANE
+
+
+
+// 03 Aufgabe
+// Erstelle eine Funktion namens "countCharacters", die einen String als Parameter übernimmt
+// und die Anzahl der Zeichen in diesem String zurückgibt.
+// Beispiel: console.log(countCharacters('banana')); // Ausgabe: 6
+
+
+
+// Knacker-Aufgabe
+// Erstelle eine Funktion namens "formatToCurrency", die eine Dezimalzahl als Parameter übernimmt
+// und diese als Währungswert im Format 0,00€ zurückgibt. Verwende die toFixed() Methode,
+// um zwei Nachkommastellen sicherzustellen und ersetze den Punkt durch ein Komma.
+// Beispiel: console.log(formatToCurrency(0.5)); // Ausgabe: "0,50€"
