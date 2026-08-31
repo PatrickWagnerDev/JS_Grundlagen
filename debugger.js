c313847bdf49eb1c34cc1070cb945c8980bfe63f
Patrick Wagner <patrickwagner2607@gmail.com>
2026-08-31 18:37:55 +0200

    add: debugger Exercice without comment

---
+++ debugger.js
@@ -0,0 +1,18 @@
+function init(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
+
+function testOne(){
+
+}
+
+function tetTwo(){
+
+}
+
+function debuggerOutput(text){
+    let debuggerLogRef = document.getElementById('debugger_log');
+    debuggerLogRef.innerHTML += text;
+}
