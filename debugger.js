072a51750160726a87f5f219276a04665861d893
Patrick Wagner <patrickwagner2607@gmail.com>
2026-08-31 19:19:09 +0200

    add: comment and link

--- debugger.js
+++ debugger.js
@@ -1,24 +1,29 @@
-let count = 0;
+// Debugger
+    // Der Debugger ist ein essentielles Tool in Chrome und anderen Browsern
+    // Die ganze Dokumentation dazu findest du hier:
+    // https://developer.chrome.com/docs/devtools/javascript?hl=de
+        
+        let count = 0;
 
-function init(){
-    testOne();
-    debuggerOutput('init');
-    testOne();
-    testTwo('testTwo');
-    testOne();
-}
+        function init(){
+            testOne();
+            debuggerOutput('init');
+            testOne();
+            testTwo('testTwo');
+            testOne();
+        }
 
-function testOne(){
-    debuggerOutput('testOne');
-}
+        function testOne(){
+            debuggerOutput('testOne');
+        }
 
-function testTwo(text){
-    debuggerOutput(text);
-    testOne();
-}
+        function testTwo(text){
+            debuggerOutput(text);
+            testOne();
+        }
 
-function debuggerOutput(text){
-    count++;
-    let debuggerLogRef = document.getElementById('debugger_log');
-    debuggerLogRef.innerHTML = text + count;
-}
+        function debuggerOutput(text){
+            count++;
+            let debuggerLogRef = document.getElementById('debugger_log');
+            debuggerLogRef.innerHTML = text + count;
+        }
