fc63de14e2bc980ffe91b8ee95a96506e3f11660
Patrick Wagner <patrickwagner2607@gmail.com>
2026-08-31 12:35:46 +0200

    add: innerHTML vs innerText

--- dom_manupulation.js
+++ dom_manupulation.js
@@ -12,3 +12,12 @@
     // Kann aber auch der Inhalt in der HTML ändern
     myTitle.innerHTML = "neuer Titel";
 
+    // Hier noch Dokumentationen zu den Basics von DOM
+    // https://www.w3schools.com/js/js_htmldom.asp
+    // https://wiki.selfhtml.org/wiki/JavaScript/DOM
+
+    // Wenn es so geschrieben wird, wird der Inhalt als HTML interpretiert, also hier als ein p-Tag
+    document.getElementById('test_div').innerHTML = '<p>test</p>';      // -> test
+
+    // Hier wird es einfach als Text interpretiert, da es als Text angegeben wird
+    document.getElementById('test_div').innerText = '<p>test</p>';      // -> <p>test</p>
